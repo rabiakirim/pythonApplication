c6cd6a4ad986ecbcfe7b531ff0bf66a06803957b
rabiakirim <rabiakirim@gmail.com>
2016-12-19 14:16:35 +0200

    Add files via upload

---
+++ ıntroductıonSınavHazırlık.py
@@ -0,0 +1,65 @@
+"""
+name= input("what is your name?")
+print ("Hello",name,"Welcome")
+"""
+
+"""
+numbers=[1,2,3,4,"bir"]
+number=[1,34,"rabia"]
+print(numbers)
+print
+print(number)
+"""
+
+"""
+arr = [['foo', 1], ['bar',2]]
+print( arr[:][:][:][:] )
+[['foo', 1], ['bar', 2]]
+"""
+
+"""
+# Create a list.
+elements = []
+
+# Append empty lists in first two indexes.
+elements.append([])
+elements.append([])
+
+# Add elements to empty lists.
+elements[0].append(1)
+elements[0].append(2)
+elements[1].append(3)
+elements[1].append(4)
+
+# Display top-left element.
+print(elements[0][0])
+
+# Display entire list.
+print(elements)
+
+# Loop over rows.
+for row in elements:
+    # Loop over columns.
+    for column in row:
+        print(column, end="")
+    print(end="\n")7
+"""
+
+"""
+values = [10,48,30, 40, 50, 60, 70]
+values.sort()
+print(values)
+values.reverse()
+print(values)
+"""
+
+"""
+#for value in values:
+    # Print each row's length and its elements.
+   # print(len(value), value)
+
+def sayHello():
+    print("hello world")
+sayHello()
+"""
+
